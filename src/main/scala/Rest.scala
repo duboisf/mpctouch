12fcecfb2e8472ce5fddecec287f58740649427d
Fred Dubois <fred.dubois@gmail.com>
2011-02-20 12:06:01 -0500

    Added some basic REST player functions
    Like play, stop, next, prev. The http method is currently GET to ease
    testing in the browser.

--- src/main/scala/Rest.scala
+++ src/main/scala/Rest.scala
@@ -1,11 +1,49 @@
-import javax.ws.rs.{GET,PUT,Produces,Path,PathParam,FormParam,WebApplicationException}
+package org.reliant.mpctouch.rest
+
+import javax.ws.rs.{GET,POST,PUT,Produces,Path,PathParam,FormParam,WebApplicationException}
 import javax.ws.rs.core.{MediaType,UriBuilder}
 
-@Path("/test")
-class Test {
+@Path("/mpd")
+@Produces(Array(MediaType.APPLICATION_JSON))
+class Playback {
+  import org.reliant.mpctouch.mpd.MpdApi.mpd.player
+
+  private val success = "{\"success\":true}"
+
+  @POST
+  @Path("/play/{pos}")
+  def play = {
+    player play 0
+    success
+  }
 
   @GET
-  @Path("/blah")
+  @Path("/playid/{id}")
+  def playid(@PathParam("id") id: Int) = {
+    player playid id
+    success
+  }
+
+  @GET
+  @Path("/stop")
   @Produces(Array(MediaType.APPLICATION_JSON))
-  def test = "{\"success\":true}"
+  def stop = {
+    player.stop
+    success
+  }
+
+  @POST
+  @Path("/next")
+  @Produces(Array(MediaType.APPLICATION_JSON))
+  def next = {
+    player.next
+    success
+  }
+
+  @POST
+  @Path("/prev")
+  def previous = {
+    player.prev
+    success
+  }
 }
