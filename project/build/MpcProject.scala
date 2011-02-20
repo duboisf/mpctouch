2f9788816e520738dca88397e307cd391f5c1f2d
Fred Dubois <fred.dubois@gmail.com>
2011-02-20 12:04:45 -0500

    Added scalampd dependency

--- project/build/MpcProject.scala
+++ project/build/MpcProject.scala
@@ -5,6 +5,7 @@ class MpcBuild(info: ProjectInfo) extends DefaultWebProject(info) {
   val jetty7Server = "org.eclipse.jetty" % "jetty-server" % "7.0.2.RC0" % "compile;test"
   val jersey = "com.sun.jersey" % "jersey-server" % "1.5"
   val jerseyJson = "com.sun.jersey" % "jersey-json" % "1.5"
+  val mpd = "org.reliant" % "scalampd_2.8.1" % "1.0" % "compile"
 
   val javaNetRepo = "Java.net Repository for Maven" at "http://download.java.net/maven/2/"
 }
---
+++ src/main/scala/Mpd.scala
@@ -0,0 +1,8 @@
+package org.reliant.mpctouch.mpd
+
+
+object MpdApi {
+  import org.reliant.mpd.Mpd
+
+  val mpd = new Mpd
+}
