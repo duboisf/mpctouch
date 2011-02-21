2db35540d3db53ed989c6ab2449f35e3a581d82b
Fred Dubois <fred.dubois@gmail.com>
2011-02-20 20:13:32 -0500

    Playing around with actors

--- src/main/scala/Mpd.scala
+++ src/main/scala/Mpd.scala
@@ -1,5 +1,22 @@
 package org.reliant.mpctouch.mpd
 
+import scala.actors.Actor
+
+case class Play(pos: Int)
+
+object MpdActor extends Actor {
+  import MpdApi.mpd.player
+
+  start
+
+  def act() {
+    loop {
+      react {
+        case Play(pos) => player play pos
+      }
+    }
+  }
+}
 
 object MpdApi {
   import org.reliant.mpd.Mpd
